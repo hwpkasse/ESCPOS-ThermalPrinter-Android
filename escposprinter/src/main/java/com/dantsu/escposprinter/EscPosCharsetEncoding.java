5187002c749c169cf1ab94d58694e831ffe5321d
Dominik Weiss <dominik@railsland.com>
2022-10-10 11:55:56 +0200

    fix

--- escposprinter/src/main/java/com/dantsu/escposprinter/EscPosCharsetEncoding.java
+++ escposprinter/src/main/java/com/dantsu/escposprinter/EscPosCharsetEncoding.java
@@ -13,7 +13,7 @@ public class EscPosCharsetEncoding {
     public EscPosCharsetEncoding(String charsetName, int escPosCharsetId) {
         this.charsetName = charsetName;
         
-        if (charsetName == "UTF-8" || charsetName == "utf-8" || charsetName == "GB18030" || charsetName == "gb18030" || charsetName == "BIG5" || charsetName == "big5") {
+        if (charsetName.equals("UTF-8") || charsetName.equals("GB18030") || charsetName.equals("BIG5")) {
             this.charsetCommand = new byte[]{0x1C, 0x26, 0x1C, 0x43, (byte) escPosCharsetId};
         } else {
             this.charsetCommand = new byte[]{0x1C, 0x2E, 0x1B, 0x74, (byte) escPosCharsetId};
